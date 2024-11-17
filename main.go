329c327f3f14768a47598a5009fe964c00108a30
khairulharu <khairulaswad719@gmail.com>
2024-11-17 20:54:31 +0800

    " auto commit bro made by khairulharu sigma "

--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
+func main() {
+	currentDirectory, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		return
+	}
+
+	file, err := os.OpenFile("readme.md", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error When Opening File: ", err)
+		return
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString("auto commit by khairulharu sheeexzzz")
+
+	if err != nil {
+		fmt.Println("Erorr writing to file: ", err)
+	}
+
+	cmdAdd := exec.Command("git", "add", ".")
+
+	cmdAdd.Dir = currentDirectory
+
+	outputAdd, err := cmdAdd.Output()
+	if err != nil {
+		fmt.Println("Error running command:", err)
+		return
+	}
+
+	fmt.Println(string(outputAdd))
+
+	cmdCommit := exec.Command("git", "commit", "-m", "\" auto commit bro made by khairulharu sigma \"")
+
+	cmdCommit.Dir = currentDirectory
+
+	outputCommit, err := cmdCommit.Output()
+	if err != nil {
+		fmt.Println("Error running command:", err)
+		return
+	}
+
+	fmt.Println(string(outputCommit))
+
+	cmdPush := exec.Command("git", "push", "origin", "master")
+
+	cmdPush.Dir = currentDirectory
+
+	outputPush, err := cmdPush.Output()
+	if err != nil {
+		fmt.Println("Error running command:", err)
+		return
+	}
+
+	fmt.Println(string(outputPush))
+
+	fmt.Println("Auto Commit To")
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+auto commit by khairulharu sheeexzzzauto commit by khairulharu sheeexzzzauto commit by khairulharu sheeexzzzauto commit by khairulharu sheeexzzz 
+auto commit by khairulharu sheeexzzzauto commit by khairulharu sheeexzzzauto commit by khairulharu sheeexzzz
